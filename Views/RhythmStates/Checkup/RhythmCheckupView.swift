8e518c08a27ca98dd29a5ea1d5001a805f93af5e
Mason Cao <s3967383@student.rmit.edu.au>
2024-02-20 18:43:26 +1100

    New Rhythm Checkup View to sound alarm if no user reponse.

---
+++ Views/RhythmStates/Checkup/RhythmCheckupView.swift
@@ -0,0 +1,107 @@
+//
+//  SwiftUIView.swift
+//
+//
+//  Created by Son Cao on 20/2/2024.
+//
+
+import SwiftUI
+
+struct RhythmCheckupView: View {
+    @Bindable var rhythmManager: RhythmManager
+    @State private var timeRemaining = 10
+    @State private var showAlert = false
+    
+    @State var alarmTimer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    func resetTimer() {
+        musicPlayer?.stop()
+        alarmTimer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+        timeRemaining = 10
+    }
+    
+    var body: some View {
+        ZStack {
+            LinearGradient(colors: [.teal, .green], startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .center, spacing: 10) {
+                Spacer()
+                Text("Have you completed")
+                    .font(.headline)
+                
+                Text("\(rhythmManager.currentTask.name)?")
+                    .font(.largeTitle.bold())
+                    .fontDesign(.rounded)
+                    .foregroundStyle(.white)
+                
+                Text("Alarm incoming: \(timeRemaining)")
+                    .foregroundStyle(.white)
+                    .onReceive(alarmTimer, perform: { _ in
+                        if timeRemaining > 0 {
+                            timeRemaining -= 1
+                        } else {
+                            alarmTimer.upstream.connect().cancel()
+                            MusicPlayer().play(file: "alarm.wav", volume: 1.0)
+                            showAlert = true
+                        }
+                    })
+                    .alert("No Response Alarm", isPresented: $showAlert) {
+                        Button("Okay", role: .cancel) {
+                            resetTimer()
+                        }
+                    } message: {
+                        Text("Don't get distracted!")
+                    }
+                
+                Spacer()
+                
+                VStack(spacing: 20) {
+                    
+                    // MARK: Go to next task
+                    Button {
+                        rhythmManager.next()
+                        rhythmManager.currentState = .active
+                    } label: {
+                        Text("Yes")
+                            .foregroundStyle(.white)
+                            .font(.title3.bold())
+                            .fontDesign(.rounded)
+                            .frame(minWidth: 100)
+                            .padding()
+                            .background(.orange.gradient)
+                            .clipShape(RoundedRectangle(cornerRadius: 15))
+                    }
+                    
+                    // MARK: Go back to current task but add 30s
+                    // TODO: Figure out this logic here
+                    Button {
+                        rhythmManager.currentState = .active
+                    } label: {
+                        Text("I need more time (+30s)")
+                            .foregroundStyle(.white)
+                            .font(.headline.bold())
+                            .fontDesign(.rounded)
+                            .padding()
+                            .background(.ultraThinMaterial)
+                            .clipShape(RoundedRectangle(cornerRadius: 15))
+                    }
+                }
+                
+                Spacer()
+            }
+            .foregroundStyle(.white)
+        }
+    }
+}
+
+#Preview {
+    MainActor.assumeIsolated {
+        let container = PreviewData.container
+        let rhythm = PreviewData.rhythmExample
+        container.mainContext.insert(rhythm)
+        
+        return RhythmCheckupView(rhythmManager: RhythmManager(tasks: rhythm.tasks))
+            .modelContainer(container)
+    }
+}
